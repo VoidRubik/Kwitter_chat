899169a052e51603c007c433a0bc1ff13cfa8645
VoidRubik <128435915+VoidRubik@users.noreply.github.com>
2023-08-09 17:38:43 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+function addUser()
+{
+      var username = document.getElementById = ("player1_name_input").value;
+      localStorage.setItem("player1_name_input",username);
+      window.location = "kwitter_room"; 
+      console.log(username);     
+}
+
+
+
